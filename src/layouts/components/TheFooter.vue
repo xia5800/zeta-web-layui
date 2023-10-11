24eb70eec3b2c56ad19a725ca79567515ecc2373
夏乀天 <ylwnpzzzn@163.com>
2023-10-11 10:11:00 +0800

    style: 部分组件文字可被选中的问题

--- src/layouts/components/TheFooter.vue
+++ src/layouts/components/TheFooter.vue
@@ -3,7 +3,7 @@ import { COPY_RIGHT } from '~/config/setting'
 </script>
 
 <template>
-  <div class="the-footer">
+  <div class="the-footer z-user-select-none">
     {{ COPY_RIGHT }}
   </div>
 </template>
